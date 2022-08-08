643aa274e74616fe75af21d8cb51a1ed1080970c
Luis <luisriveros26@gmail.com>
2022-08-07 20:04:44 -0600

    finished schemas and started mutations.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: String,
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook (authors: [String], title: String, bookId: String, description:String, image: String, link: String): User
+        removeBook (bookId: String!): User
+    }
+
+`;
+
+module.exports = typeDefs;
